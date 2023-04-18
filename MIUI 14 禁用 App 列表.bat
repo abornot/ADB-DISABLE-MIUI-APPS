b37885706c96e91890a3f09266dcc9866c45cb6f
abornot <117444566+abornot@users.noreply.github.com>
2023-04-18 15:56:11 +0800

    Add files via upload

--- MIUI 14 禁用 App 列表.bat
+++ MIUI 14 禁用 App 列表.bat
@@ -1,46 +1,24 @@
-# 小米系统广告解决方案
 ./adb shell pm disable-user com.miui.systemAdSolution
-# 小米广告分析
 ./adb shell pm disable-user com.miui.analytics
-# 游戏服务
 ./adb shell pm disable-user com.xiaomi.gamecenter.sdk.service
-# 搜狗输入法小米版
 ./adb shell pm disable-user com.sohu.inputmethod.sogou.xiaomi
-# 音乐
 ./adb shell pm disable-user com.miui.player
-# 小米视频
 ./adb shell pm disable-user com.miui.video
-# 金山翻译
 ./adb shell pm disable-user com.miui.translation.kingsoft
-# 电子邮件
 ./adb shell pm disable-user com.android.email
-# 快应用服务框架
 ./adb shell pm disable-user com.miui.hybrid
-# 用户反馈
 ./adb shell pm disable-user com.miui.bugreport
-# 投屏
 ./adb shell pm disable-user com.milink.service
-# 浏览器
 ./adb shell pm disable-user com.android.browser
-# 生活黄页
 ./adb shell pm disable-user com.miui.yellowpage
-# 米币支付
 ./adb shell pm disable-user com.xiaomi.payment
-# 小爱同学
 ./adb shell pm disable-user com.miui.voiceassist
-# 悬浮球
 ./adb shell pm disable-user com.miui.touchassistant
-# 服务与反馈
 ./adb shell pm disable-user com.miui.miservice
-# 小米商城系统组件
 ./adb shell pm disable-user com.xiaomi.ab
-# AI虚拟助手
 ./adb shell pm disable-user com.xiaomi.aiasst.service
-# Joyose
-# ./adb shell pm disable-user com.xiaomi.joyose
-# 小米互联通信服务
+::./adb shell pm disable-user com.xiaomi.joyose
 ./adb shell pm disable-user com.xiaomi.mi_connect_service
-# 小米支付
 ./adb shell pm disable-user com.miui.nextpay
-# 智能助理
 ./adb shell pm disable-user com.miui.personalassistant
+exit
